f819157a34aa1cf68ca8c832895541c72d5cd720
Robert-2360 <ROBERT40@live.seminolestate.edu>
2015-09-30 21:41:17 -0400

    Exercise 8.12

---
+++ Duplicate Elimination/DuplicateElimination.cs
@@ -0,0 +1,75 @@
+﻿// Exercise 8.12: Duplicate Elimination
+// Eliminate duplicate entries from console input
+// Assumptions:
+//    Entries are all integers from 10 to 100 inclusive.
+//    No more than five unique numbers are saved.
+//    The array is not sorted.
+
+using System;
+
+public class DuplicateElimination
+{
+   public static void Main(string[] args)
+   {
+      // initialize constants
+      const int SIZE = 5;
+
+      // initialize varibles
+      int[] numbers = new int[SIZE];
+
+      // loop until numbers array is "full"
+      while (numbers[SIZE-1] == 0)
+      {
+         // User prompt
+         Console.Write("Enter a number between 10 and 100, inclusive: ");
+         int number = Convert.ToInt32(Console.ReadLine());
+
+         AddNumberIfUnique(number, numbers);
+
+         //Display results
+         Console.Write("Unique numbers entered so far: ");
+         DisplayArray(numbers);
+         Console.WriteLine();
+      }
+
+      // Display final results
+      Console.WriteLine("You have entered {0} unique numbers.", SIZE);
+      Console.Write("Here they are again: ");
+      DisplayArray(numbers);
+
+      // Freeze console window
+      Console.ReadLine();
+   }
+
+
+   // Display the numbers in the array, stop at first zero
+   public static void DisplayArray(int[] anArray)
+   {
+      for (int i = 0; i < anArray.Length; i++)
+      {
+         if (anArray[i] != 0) Console.Write("{0}  ", anArray[i]);
+         else break;
+      }
+      Console.WriteLine();
+   }
+
+
+   // Include the given number in the array only if it is unique
+   public static void AddNumberIfUnique(int aNumber, int[] anArray)
+   {
+      for (int i = 0; i < anArray.Length; i++)
+      {
+         // Check if the array slot is zero, this means the number is unique
+         if (anArray[i] == 0)
+         {
+            anArray[i] = aNumber;
+            break;
+         }
+
+         // Check if the number is a duplicate entry, if so stop looking
+         if (anArray[i] == aNumber) break;
+      }
+   }
+
+
+}
