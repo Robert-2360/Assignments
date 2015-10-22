602cb94c3cd8c0fbddf49ac73d71cc64ebc7b208
Robert-2360 <ROBERT40@live.seminolestate.edu>
2015-10-22 08:45:04 -0400

    Exercise 10.8

---
+++ Set of Integers/IntegerSet.cs
@@ -0,0 +1,95 @@
+﻿// Exercise 10.8: Set of Integers
+// Represents a set of integers from 0 to 100 by setting each element to true or false
+
+using System;
+
+class IntegerSet
+{
+   // Initialize constants
+   public static int LOW_RANGE = 0;
+   public static int HIGH_RANGE = 100;
+
+   // Declare class variables
+   private bool[] _arraySet;
+   public bool[] ArraySet
+   {
+      get { return _arraySet; }
+      private set { _arraySet = value; }
+   }
+
+   // Constructor that sets all elements to false
+   public IntegerSet()
+   {
+      ArraySet = new bool[HIGH_RANGE - LOW_RANGE + 1];
+      for (int i = LOW_RANGE; i <= HIGH_RANGE; i++) ArraySet[i] = false;
+   }
+
+   // ToString method
+   public override string ToString()
+   {
+      string result = "";
+      for (int i = LOW_RANGE; i <= HIGH_RANGE; i++)
+      {
+         if (ArraySet[i]) result += i + " ";
+      }
+      if (result == "") result = "---";
+      return result;
+   }
+
+   // Return true if argument is inside range bonudaries
+   private bool isValid(int i)
+   {
+      return (LOW_RANGE <= i) && (i <= HIGH_RANGE);
+   }
+
+   // Sets a given element to true
+   public void InsertElement(int k)
+   {
+      if (!isValid(k)) throw new IndexOutOfRangeException();
+      ArraySet[k] = true;
+   }
+
+   // Sets a given element to false
+   public void DeleteElement(int m)
+   {
+      if (!isValid(m)) throw new IndexOutOfRangeException();
+      ArraySet[m] = false;
+   }
+
+   // Returns an IntegerSet where each element is true if it true in either or both given sets
+   public IntegerSet Union(IntegerSet that)
+   {
+      IntegerSet resultSet = new IntegerSet();
+      for (int i = LOW_RANGE; i <= HIGH_RANGE; i++)
+      {
+         if (this.ArraySet[i] || that.ArraySet[i]) resultSet.InsertElement(i);
+      }
+      return resultSet;
+   }
+
+   // Returns an IntegerSet where each element is true only if it true in both given sets
+   public IntegerSet Intersection(IntegerSet that)
+   {
+      IntegerSet resultSet = new IntegerSet();
+      for (int i = LOW_RANGE; i <= HIGH_RANGE; i++)
+      {
+         if (this.ArraySet[i] && that.ArraySet[i]) resultSet.InsertElement(i);
+      }
+      return resultSet;
+   }
+
+   // Returns true if every element in both given set have the same value
+   public bool IsEqualTo(IntegerSet that)
+   {
+      bool result = true;
+      for (int i = LOW_RANGE; i <= HIGH_RANGE; i++)
+      {
+         if (this.ArraySet[i] != that.ArraySet[i])
+         {
+            result = false;
+            break;
+         }
+      }
+      return result;
+   }
+}
---
+++ Set of Integers/IntegerSetTest.cs
@@ -0,0 +1,77 @@
+﻿// Exercise 10.8: Set of Integers
+// A test class for the IntegerSet class
+
+using System;
+
+class IntegerSetTest
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("Low Range: {0}", IntegerSet.LOW_RANGE);
+      Console.WriteLine("High Range: {0}", IntegerSet.HIGH_RANGE);
+      Console.WriteLine();
+
+      try
+      {
+         // Test for the empty set
+         IntegerSet empty = new IntegerSet();
+         Console.WriteLine("Empty set: {0} \n", empty);
+
+         // Test InsertElement method is set A
+         IntegerSet setA = new IntegerSet();
+         setA.InsertElement(0);
+         setA.InsertElement(1);
+         setA.InsertElement(50);
+         setA.InsertElement(100);
+         Console.WriteLine("Insert elements in Set A: {0}", setA);
+
+         // Test InsertElement method is set B
+         IntegerSet setB = new IntegerSet();
+         setB.InsertElement(100);
+         setB.InsertElement(99);
+         setB.InsertElement(50);
+         setB.InsertElement(0);
+         Console.WriteLine("Insert elements in Set B: {0} \n", setB);
+
+         // Test DeleteElement method in set A and B
+         setA.DeleteElement(100);
+         setA.DeleteElement(99); // element not in set
+         setB.DeleteElement(0);
+         Console.WriteLine("Delete 100 from Set A: {0}", setA);
+         Console.WriteLine("Delete 0 from Set B: {0} \n", setB);
+
+         // Test Union method for set A and B
+         IntegerSet unionSet = setA.Union(setB);
+         Console.WriteLine("Union of A and B: {0} \n", unionSet);
+
+         // Test Intersection method for set A and B 
+         IntegerSet intersectSet = setA.Intersection(setB);
+         Console.WriteLine("Intersection of A and B: {0} \n", intersectSet);
+
+         // Test IsEqualTo method for set A, B, and C
+         IntegerSet setC = new IntegerSet();
+         setC.InsertElement(0);
+         setC.InsertElement(1);
+         setC.InsertElement(50);
+         Console.WriteLine("Set A: {0}", setA);
+         Console.WriteLine("Set B: {0}", setB);
+         Console.WriteLine("Set C: {0}", setC);
+         Console.WriteLine("Is Set A equal to Set B? {0}", setA.IsEqualTo(setB));
+         Console.WriteLine("Is Set A equal to Set C? {0} \n", setA.IsEqualTo(setC));
+
+         // Test isValid private method
+         Console.WriteLine("Try to insert element 999");
+         setA.InsertElement(999);
+      }
+
+      catch (IndexOutOfRangeException ex)
+      {
+         Console.WriteLine("*******************************************");
+         Console.WriteLine(ex.Message);
+         Console.WriteLine("*******************************************");
+      }
+
+      // Freeze console window
+      Console.ReadLine();
+   }
+}
