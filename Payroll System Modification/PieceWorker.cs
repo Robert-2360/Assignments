c12cc1e66c5525cfb424d3cc3ae4d01f5a9f0389
Robert-2360 <ROBERT40@live.seminolestate.edu>
2015-11-20 04:01:40 -0500

    Exercise 12.11

---
+++ Payroll System Modification/PieceWorker.cs
@@ -0,0 +1,53 @@
+﻿// Exercise 12.11: Payroll System Modification
+
+using System;
+
+class PieceWorker : Employee
+{
+   private decimal _wage;
+   private int _pieces;
+
+   public decimal Wage
+   {
+      get
+      {
+         return _wage;
+      }
+      set
+      {
+         if (value >= 0) _wage = value;
+         else throw new ArgumentOutOfRangeException("Wage", value, "Wage must be >= 0");
+      }
+   }
+
+   public int Pieces
+   {
+      get
+      {
+         return _pieces;
+      }
+      set
+      {
+         if (value >= 0) _pieces = value;
+         else throw new ArgumentOutOfRangeException("Pieces", value, "Pieces must be >= 0");
+      }
+   }
+
+   public PieceWorker(string first, string last, string ssn, decimal wage, int pieces)
+      : base(first, last, ssn)
+   {
+      Wage = wage;
+      Pieces = pieces;
+   }
+
+   public override decimal Earnings()
+   {
+      return Wage * Pieces;
+   }
+
+   public override string ToString()
+   {
+      return string.Format("piece worker: {0}\n{1}: {2:C}; {3}: {4}",
+         base.ToString(), "wage", Wage, "pieces", Pieces);
+   }
+}
